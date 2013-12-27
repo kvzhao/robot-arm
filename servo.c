b89dc797df1b4a4104523a0a2f92266cc7d79f57
kvzhao <kevinzhaio@gmail.com>
2013-12-27 23:55:07 +0800

    Change back to TIM4, previous commit is an error

--- servo.c
+++ servo.c
@@ -1,9 +1,9 @@
 #include "servo.h"
 
-void TIM2_PWM_Configuration()
+void TIM4_PWM_Configuration()
 {
     /* RCC Initialization */
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
     /* Timer 4 Initialization */
@@ -19,31 +19,31 @@ void TIM2_PWM_Configuration()
     TIM_TimeBaseInitStruct.TIM_Period = 3760 - 1;
     TIM_TimeBaseInitStruct.TIM_Prescaler = 500 - 1;
     TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseInit( TIM2, &TIM_TimeBaseInitStruct );
+    TIM_TimeBaseInit( TIM4, &TIM_TimeBaseInitStruct );
 
     TIM_OCStructInit( &TIM_OCInitStruct );
     TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
     TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;
 
     // Initial duty cycle equals 0%. Value can range from zero to 65535.
-    // TIM_Pulse = TIM2_CCR1 register (16 bits)
+    // TIM_Pulse = TIM4_CCR1 register (16 bits)
     TIM_OCInitStruct.TIM_Pulse = 65535;
     //(0=Always Off, 65535=Always On)
 
-    TIM_OC1Init( TIM2, &TIM_OCInitStruct ); // Channel 1  LED
-    TIM_OC2Init( TIM2, &TIM_OCInitStruct ); // Channel 2  LED
-    TIM_OC3Init( TIM2, &TIM_OCInitStruct ); // Channel 3  LED
-    TIM_OC4Init( TIM2, &TIM_OCInitStruct ); // Channel 4  LED
+    TIM_OC1Init( TIM4, &TIM_OCInitStruct ); // Channel 1  LED
+    TIM_OC2Init( TIM4, &TIM_OCInitStruct ); // Channel 2  LED
+    TIM_OC3Init( TIM4, &TIM_OCInitStruct ); // Channel 3  LED
+    TIM_OC4Init( TIM4, &TIM_OCInitStruct ); // Channel 4  LED
 
-    TIM_Cmd( TIM2, ENABLE );
+    TIM_Cmd( TIM4, ENABLE );
 
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_StructInit(&GPIO_InitStructure); // Reset init structure
 
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM2);
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM2);
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM2);
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM2);
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
 
     // Setup Blue & Green LED on STM32-Discovery Board to use PWM.
     GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
@@ -57,7 +57,7 @@ void TIM2_PWM_Configuration()
 void Servo_Configuration()
 {
     /* Set the period of PWM is 20 ms (f=50 Hz) */
-    TIM2_PWM_Configuration();
+    TIM4_PWM_Configuration();
 }
 
 void Servo_set_pos (uint8_t angle, uint8_t ch)
@@ -65,16 +65,16 @@ void Servo_set_pos (uint8_t angle, uint8_t ch)
     uint32_t cval = 3760 / (10 + 10*angle/180);
     switch (ch) {
         case 0:
-            TIM2->CCR1 = cval;
+            TIM4->CCR1 = cval;
             break;
         case 1:
-            TIM2->CCR2 = cval;
+            TIM4->CCR2 = cval;
             break;
         case 2:
-            TIM2->CCR3 = cval;
+            TIM4->CCR3 = cval;
             break;
         case 3:
-            TIM2->CCR4 = cval;
+            TIM4->CCR4 = cval;
             break;
     }
     vTaskDelay(50);
